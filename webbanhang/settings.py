f760ecebc2e72d725e91843a1a8f4d0afb1f89c9
Tran Van Thai <thait@THAITV09.EVNCPC.VN>
2023-10-27 11:08:18 +0700

    Create project

--- webbanhang/settings.py
+++ webbanhang/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,6 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(local())
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
