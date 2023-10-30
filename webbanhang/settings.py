e4899be43e95678b82ef39136b21652296e69118
Tran Van Thai <thait@THAITV09.EVNCPC.VN>
2023-10-30 09:30:58 +0700

    Create project

--- webbanhang/settings.py
+++ webbanhang/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(local())
+django_heroku.settings(locals())
 
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
